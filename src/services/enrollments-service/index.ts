2d18911289512d6877cd313a7985487250b1dc14
Isabela <isabela_cmoreira@hotmail.com>
2022-11-14 16:08:51 -0300

    feat: implemented cep validation

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -1,5 +1,5 @@
 import { request } from '@/utils/request';
-import { notFoundError } from '@/errors';
+import { notFoundError, requestError } from '@/errors';
 import addressRepository, { CreateAddressParams } from '@/repositories/address-repository';
 import enrollmentRepository, { CreateEnrollmentParams } from '@/repositories/enrollment-repository';
 import { exclude } from '@/utils/prisma-utils';
@@ -51,8 +51,11 @@ type GetAddressResult = Omit<Address, 'createdAt' | 'updatedAt' | 'enrollmentId'
 async function createOrUpdateEnrollmentWithAddress(params: CreateOrUpdateEnrollmentWithAddress) {
   const enrollment = exclude(params, 'address');
   const address = getAddressForUpsert(params.address);
+  const cepSearch = await request.get(`https://viacep.com.br/ws/${address.cep}/json/`);
+  const cepValidation = cepSearch.data.cep;
 
-  //TODO - Verificar se o CEP é válido
+  if (!cepValidation) throw requestError;
+
   const newEnrollment = await enrollmentRepository.upsert(params.userId, enrollment, exclude(enrollment, 'userId'));
 
   await addressRepository.upsert(newEnrollment.id, address, address);
